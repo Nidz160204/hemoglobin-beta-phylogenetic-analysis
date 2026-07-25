854529b564711e7e9353daf2ee0ecb2403aa14ee
NIDHI SHAH <ns9769@srmist.edu.in>
2026-07-25 23:21:47 +0530

    Update muscle_alignment.sh

--- scripts/muscle_alignment.sh
+++ scripts/muscle_alignment.sh
@@ -1,8 +1,21 @@
-# Multiple Sequence Alignment
+#!/bin/bash
 
-# Alignment performed using MUSCLE Web Server
+# ==========================================================
+# Multiple Sequence Alignment using MUSCLE
+# Project:
+# Evolutionary Analysis of Hemoglobin Beta (HBB)
+# ==========================================================
+
 # Input:
 # data/combined/HBB_vertebrates.fasta
 
+# Method:
+# MUSCLE was executed using the EMBL-EBI MUSCLE web server
+# https://www.ebi.ac.uk/jdispatcher/msa/muscle
+
 # Output:
 # data/aligned/HBB_vertebrates_aligned.fasta
+
+echo "Multiple sequence alignment performed using the MUSCLE web server."
+echo "Input : data/combined/HBB_vertebrates.fasta"
+echo "Output: data/aligned/HBB_vertebrates_aligned.fasta"
