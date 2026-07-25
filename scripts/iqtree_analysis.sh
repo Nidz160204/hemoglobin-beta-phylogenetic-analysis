f86a71f78e538fe78a0be6543d03ab6bd0e6885b
NIDHI SHAH <ns9769@srmist.edu.in>
2026-07-25 23:22:07 +0530

    Update iqtree_analysis.sh

--- scripts/iqtree_analysis.sh
+++ scripts/iqtree_analysis.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# IQ-TREE Maximum Likelihood Analysis
+# ==========================================================
+# IQ-TREE Maximum Likelihood Phylogenetic Analysis
+# Project:
+# Evolutionary Analysis of Hemoglobin Beta (HBB)
+# ==========================================================
+
+# Input alignment
+INPUT="data/aligned/HBB_vertebrates_aligned.fasta"
+
+echo "Running IQ-TREE analysis..."
 
 iqtree3 \
--s data/aligned/HBB_vertebrates_aligned.fasta \
+-s "$INPUT" \
 -st AA \
 -m MFP \
 -bb 1000
+
+echo "Analysis complete."
