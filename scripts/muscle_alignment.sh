9002f03d9f287b1b6caea6e2b971fc0e2fabcefd
NIDHI SHAH <ns9769@srmist.edu.in>
2026-07-25 23:19:39 +0530

    Create muscle_alignment.sh

---
+++ scripts/muscle_alignment.sh
@@ -0,0 +1,8 @@
+# Multiple Sequence Alignment
+
+# Alignment performed using MUSCLE Web Server
+# Input:
+# data/combined/HBB_vertebrates.fasta
+
+# Output:
+# data/aligned/HBB_vertebrates_aligned.fasta
